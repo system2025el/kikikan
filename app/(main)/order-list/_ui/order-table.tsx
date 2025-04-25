5bf01209fb8fa2c05d33f5148838d51132430f12
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-04-25 11:25:46 +0900

    st 受注一覧画面　daterangepickerのデフォルト問題残っている

---
+++ app/(main)/order-list/_ui/order-table.tsx
@@ -0,0 +1,159 @@
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import {
+  Box,
+  Button,
+  Checkbox,
+  IconButton,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  useTheme,
+} from '@mui/material';
+import { grey } from '@mui/material/colors';
+import React, { useState } from 'react';
+
+import { orderList } from '../../../_lib/mock-data';
+
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (event: React.MouseEvent<HTMLButtonElement>, newPage: number) => void;
+}
+
+export const TablePaginationActions = (props: TablePaginationActionsProps) => {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton onClick={handleFirstPageButtonClick} disabled={page === 0} aria-label="first page">
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+};
+
+/** 受注一覧テーブルのコンポーネント */
+export const OrderTable = () => {
+  const [page, setPage] = useState(0);
+  const rowsPerPage = 20;
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - orderList.length) : 0;
+
+  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    setPage(newPage);
+  };
+  // const handleChangePage = (event: unknown, newPage: number) => {
+  //   setPage(newPage);
+  // };
+
+  return (
+    <>
+      <TableContainer component={Paper} variant="outlined" square sx={{ p: 2, maxHeight: 800, bgcolor: grey[200] }}>
+        <Stack>
+          <Box width={300} justifyItems={'end'}>
+            <TablePagination
+              count={orderList.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              ActionsComponent={TablePaginationActions}
+              rowsPerPageOptions={[20]}
+              sx={{ bgcolor: grey[200] }}
+            />
+          </Box>
+          <Box width={'50%'}></Box>
+          <Button color="error" sx={{ ml: '40%' }}>
+            -削除
+          </Button>
+          <Button>マージ</Button>
+          <Button>コピー</Button>
+        </Stack>
+        <Table stickyHeader size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ bgcolor: grey[300] }}></TableCell>
+              <TableCell sx={{ bgcolor: grey[300] }}>受注番号</TableCell>
+              <TableCell sx={{ bgcolor: grey[300] }}>受注ステータス</TableCell>
+              <TableCell sx={{ bgcolor: grey[300] }}>公演名</TableCell>
+              <TableCell sx={{ bgcolor: grey[300] }}>公演場所</TableCell>
+              <TableCell sx={{ bgcolor: grey[300] }}>顧客名</TableCell>
+              <TableCell sx={{ bgcolor: grey[300] }}>受注日</TableCell>
+              <TableCell sx={{ bgcolor: grey[300] }}>出庫日</TableCell>
+              <TableCell sx={{ bgcolor: grey[300] }}>返却日</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0 ? orderList.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : orderList).map(
+              (order) => (
+                <TableRow key={order.id}>
+                  <TableCell>
+                    <Checkbox color="primary" />
+                  </TableCell>
+                  <TableCell>
+                    <Button variant="text">{order.orderNumber}</Button>
+                  </TableCell>
+                  <TableCell>{order.status}</TableCell>
+                  <TableCell>{order.name}</TableCell>
+                  <TableCell>{order.location}</TableCell>
+                  <TableCell>{order.customerName}</TableCell>
+                  <TableCell>{order.orderedDate}</TableCell>
+                  <TableCell>{order.issueDate}</TableCell>
+                  <TableCell>{order.returnDate}</TableCell>
+                </TableRow>
+              )
+            )}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={8} />
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
