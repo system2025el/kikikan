0ace96e1434f23d6336266e6ac4f74def489115b
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-04-30 14:17:21 +0900

    st　地域選択をダイアログで作成

---
+++ app/(main)/new-order/location-selection/_lib/data.ts
@@ -0,0 +1,279 @@
+/** モック用データ */
+export const areaCate = [
+  { id: 1, name: '北海道' },
+  { id: 2, name: '東北' },
+  { id: 3, name: '関東' },
+  { id: 4, name: '東海' },
+  { id: 5, name: '信越' },
+  { id: 6, name: '近畿' },
+  { id: 7, name: '中国' },
+  { id: 8, name: '四国' },
+  { id: 9, name: '九州' },
+  { id: 10, name: '沖縄' },
+  { id: 11, name: '分類11' },
+  { id: 12, name: '分類12' },
+  { id: 13, name: '分類13' },
+];
+
+type Area = {
+  name: string;
+  cateId: number;
+};
+export const areaList: Area[] = [
+  {
+    name: '北海道',
+    cateId: 1,
+  },
+  {
+    name: '沖縄',
+    cateId: 10,
+  },
+  {
+    name: '青森',
+    cateId: 2,
+  },
+  {
+    name: '秋田',
+    cateId: 2,
+  },
+  {
+    name: '山形',
+    cateId: 2,
+  },
+  {
+    name: '福島',
+    cateId: 2,
+  },
+  {
+    name: '宮城',
+    cateId: 2,
+  },
+  {
+    name: '岩手',
+    cateId: 2,
+  },
+  {
+    name: '東京',
+    cateId: 3,
+  },
+  {
+    name: '都下',
+    cateId: 3,
+  },
+  {
+    name: '神奈川',
+    cateId: 3,
+  },
+  {
+    name: '埼玉',
+    cateId: 3,
+  },
+  {
+    name: '千葉',
+    cateId: 3,
+  },
+  {
+    name: '茨城',
+    cateId: 3,
+  },
+  {
+    name: '山梨',
+    cateId: 3,
+  },
+  {
+    name: '群馬',
+    cateId: 3,
+  },
+  {
+    name: '栃木',
+    cateId: 3,
+  },
+  {
+    name: '静岡',
+    cateId: 4,
+  },
+  {
+    name: '愛知',
+    cateId: 4,
+  },
+  {
+    name: '岐阜',
+    cateId: 4,
+  },
+  {
+    name: '三重',
+    cateId: 4,
+  },
+  {
+    name: '新潟',
+    cateId: 5,
+  },
+  {
+    name: '長野',
+    cateId: 5,
+  },
+  {
+    name: '富山',
+    cateId: 5,
+  },
+  {
+    name: '石川',
+    cateId: 5,
+  },
+  {
+    name: '福井',
+    cateId: 5,
+  },
+  {
+    name: '大阪',
+    cateId: 6,
+  },
+  {
+    name: '京都',
+    cateId: 6,
+  },
+  {
+    name: '兵庫',
+    cateId: 6,
+  },
+  {
+    name: '滋賀',
+    cateId: 6,
+  },
+  {
+    name: '奈良',
+    cateId: 6,
+  },
+  {
+    name: '和歌山',
+    cateId: 6,
+  },
+  {
+    name: '広島',
+    cateId: 7,
+  },
+  {
+    name: '岡山',
+    cateId: 7,
+  },
+  {
+    name: '山口',
+    cateId: 7,
+  },
+  {
+    name: '島根',
+    cateId: 7,
+  },
+  {
+    name: '鳥取',
+    cateId: 7,
+  },
+  {
+    name: '徳島',
+    cateId: 8,
+  },
+  {
+    name: '高知',
+    cateId: 8,
+  },
+  {
+    name: '愛媛',
+    cateId: 8,
+  },
+  {
+    name: '香川',
+    cateId: 8,
+  },
+  {
+    name: '福岡',
+    cateId: 9,
+  },
+  {
+    name: '長崎',
+    cateId: 9,
+  },
+  {
+    name: '鹿児島',
+    cateId: 9,
+  },
+  {
+    name: '熊本',
+    cateId: 9,
+  },
+  {
+    name: '宮崎',
+    cateId: 9,
+  },
+  {
+    name: '大分',
+    cateId: 9,
+  },
+  {
+    name: '佐賀',
+    cateId: 9,
+  },
+  {
+    name: '船舶',
+    cateId: 11,
+  },
+  {
+    name: 'その他',
+    cateId: 11,
+  },
+  {
+    name: '海外',
+    cateId: 11,
+  },
+  {
+    name: 'アジア',
+    cateId: 12,
+  },
+  {
+    name: '欧州',
+    cateId: 12,
+  },
+  {
+    name: '北米',
+    cateId: 12,
+  },
+  {
+    name: '南米',
+    cateId: 12,
+  },
+  {
+    name: '中東',
+    cateId: 12,
+  },
+  {
+    name: 'アフリカ',
+    cateId: 12,
+  },
+  {
+    name: 'オセアニア',
+    cateId: 12,
+  },
+  {
+    name: '中華人民共和国',
+    cateId: 13,
+  },
+  {
+    name: '韓国',
+    cateId: 13,
+  },
+  {
+    name: '米国',
+    cateId: 13,
+  },
+  {
+    name: '英国',
+    cateId: 13,
+  },
+  {
+    name: 'フランス',
+    cateId: 13,
+  },
+  {
+    name: 'イタリア',
+    cateId: 13,
+  },
+];
--- app/(main)/new-order/location-selection/_ui/area-selection-dialog.tsx
+++ app/(main)/new-order/location-selection/_ui/area-selection-dialog.tsx
@@ -1,14 +1,45 @@
 import { Box, Button, DialogContent, DialogTitle, Grid2, Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
+import { areaCate, areaList } from '../_lib/data';
 
 export const AreaSelectionDialog = () => {
   return (
     <>
-      <DialogTitle bgcolor={grey[300]} display={'flex'} justifyContent={'space-between'}>
+      <DialogTitle bgcolor={grey[300]} display={'flex'}>
         地域選択
-        <Button href="/new-order/location-selection">戻る</Button>
+        <Button href="/new-order/location-selection" sx={{ ml: '90%' }}>
+          戻る
+        </Button>
       </DialogTitle>
-      <DialogContent></DialogContent>
+      <Box bgcolor={grey[300]} pl={'30%'}>
+        <Typography>地域名を選択してください。</Typography>
+      </Box>
+      <DialogContent>
+        <Grid2 container borderBottom={1}>
+          <Grid2 size={5} textAlign={'center'}>
+            地域分類
+          </Grid2>
+          <Grid2 size={7}>地域名</Grid2>
+        </Grid2>
+        {areaCate.map((cate) => (
+          <Grid2 key={cate.id} container borderBottom={1}>
+            <Grid2 size={5} textAlign={'center'}>
+              {cate.name}
+            </Grid2>
+            <Grid2 size={7}>
+              {areaList
+                .filter((area) => area.cateId === cate.id)
+                .map((area) => (
+                  <Box key={area.name}>
+                    <Button variant="text" size="large">
+                      {area.name}
+                    </Button>
+                  </Box>
+                ))}
+            </Grid2>
+          </Grid2>
+        ))}
+      </DialogContent>
     </>
   );
 };
@@ -21,243 +52,3 @@ const styles: { [key: string]: React.CSSProperties } = {
     marginTop: 2,
   },
 };
-/** モック用データ */
-type Area = {
-  name: string;
-  size: number;
-};
-const area1List: Area[] = [
-  {
-    name: '北海道',
-    size: 1.5,
-  },
-  {
-    name: '沖縄',
-    size: 3,
-  },
-  {
-    name: '全て',
-    size: 1.5,
-  },
-];
-const tohokuList: Area[] = [
-  {
-    name: '青森',
-    size: 1.5,
-  },
-  {
-    name: '秋田',
-    size: 1.5,
-  },
-  {
-    name: '山形',
-    size: 1.5,
-  },
-  {
-    name: '福島',
-    size: 1.5,
-  },
-  {
-    name: '宮城',
-    size: 1.5,
-  },
-  {
-    name: '岩手',
-    size: 1.5,
-  },
-];
-const tokyoList: Area[] = [
-  {
-    name: '東京',
-    size: 1.5,
-  },
-  {
-    name: '都下',
-    size: 1.5,
-  },
-];
-const kantoList: Area[] = [
-  {
-    name: '神奈川',
-    size: 1.5,
-  },
-  {
-    name: '埼玉',
-    size: 1.5,
-  },
-  {
-    name: '千葉',
-    size: 1.5,
-  },
-  {
-    name: '茨城',
-    size: 1.5,
-  },
-  {
-    name: '山梨',
-    size: 1.5,
-  },
-  {
-    name: '群馬',
-    size: 1.5,
-  },
-  {
-    name: '栃木',
-    size: 1.5,
-  },
-];
-
-const tokaiList: Area[] = [
-  {
-    name: '静岡',
-    size: 1.5,
-  },
-  {
-    name: '愛知',
-    size: 1.5,
-  },
-  {
-    name: '岐阜',
-    size: 1.5,
-  },
-  {
-    name: '三重',
-    size: 1.5,
-  },
-];
-
-const shinetsuList: Area[] = [
-  {
-    name: '新潟',
-    size: 1.5,
-  },
-  {
-    name: '長野',
-    size: 1.5,
-  },
-];
-
-const hokurikuList: Area[] = [
-  {
-    name: '富山',
-    size: 1.5,
-  },
-  {
-    name: '石川',
-    size: 1.5,
-  },
-  {
-    name: '福井',
-    size: 1.5,
-  },
-];
-const kinkiList: Area[] = [
-  {
-    name: '大阪',
-    size: 1.5,
-  },
-  {
-    name: '京都',
-    size: 1.5,
-  },
-  {
-    name: '兵庫',
-    size: 1.5,
-  },
-  {
-    name: '滋賀',
-    size: 1.5,
-  },
-  {
-    name: '奈良',
-    size: 1.5,
-  },
-  {
-    name: '和歌山',
-    size: 1.5,
-  },
-];
-const chugokuList: Area[] = [
-  {
-    name: '広島',
-    size: 1.5,
-  },
-  {
-    name: '岡山',
-    size: 1.5,
-  },
-  {
-    name: '山口',
-    size: 1.5,
-  },
-  {
-    name: '島根',
-    size: 1.5,
-  },
-  {
-    name: '鳥取',
-    size: 1.5,
-  },
-];
-const shiokokuList: Area[] = [
-  {
-    name: '徳島',
-    size: 1.5,
-  },
-  {
-    name: '高知',
-    size: 1.5,
-  },
-  {
-    name: '愛媛',
-    size: 1.5,
-  },
-  {
-    name: '香川',
-    size: 1.5,
-  },
-];
-const kyushuList: Area[] = [
-  {
-    name: '福岡',
-    size: 1.5,
-  },
-  {
-    name: '長崎',
-    size: 1.5,
-  },
-  {
-    name: '鹿児島',
-    size: 1.5,
-  },
-  {
-    name: '熊本',
-    size: 1.5,
-  },
-  {
-    name: '宮崎',
-    size: 1.5,
-  },
-  {
-    name: '大分',
-    size: 1.5,
-  },
-  {
-    name: '佐賀',
-    size: 1.5,
-  },
-];
-const area2List: Area[] = [
-  {
-    name: '船舶',
-    size: 1.5,
-  },
-  {
-    name: 'その他',
-    size: 1.5,
-  },
-  {
-    name: '海外',
-    size: 7.5,
-  },
-];
