ad6e8a7a94c3177c97abc92043c4b1f13f70ddab
DESKTOP-JPKNAE4\s.kashiwai <s.kashiwai@refact.co.jp>
2025-07-03 19:22:29 +0900

    貸出状況　機材貸出スケジュール画面追加

--- app/(main)/loan-situation/[kizai_id]/_lib/colorselect.ts
+++ app/(main)/loan-situation/[kizai_id]/_lib/colorselect.ts
@@ -1,39 +1,20 @@
-export const getLoanHeaderBackgroundColor = (index: number, colorSelect: boolean): string => {
-  return 'lightgrey';
-};
-
-export const getLoanRowBackgroundColor = (rowIndex: number, colIndex: number, colorSelect: boolean): string => {
-  if (colIndex === 5) {
-    return 'lightgrey';
-  } else if (rowIndex === 9) {
-    return 'lightgrey';
+export const getDateRowBackgroundColor = (juchuHonbanbiShubetuId: number) => {
+  switch (juchuHonbanbiShubetuId) {
+    case 4:
+      return 'pink';
+    case 3:
+      return 'lightgreen';
+    case 2:
+      return 'orange';
+    case 1:
+      return 'mediumpurple';
+    case 7:
+      return 'yellow';
+    case 6:
+      return 'lightblue';
+    case 5:
+      return '#ACB9CA';
+    default:
+      return 'white';
   }
-  return 'white';
-};
-
-export const getLoanTextColor = (index: number, colorSelect: boolean): string => {
-  return 'black';
-};
-
-export const getDateHeaderBackgroundColor = (index: number, colorSelect: boolean): string => {
-  return 'black';
-};
-
-export const getDateHeaderTextColor = (index: number, colorSelect: boolean): string => {
-  return 'white';
-};
-
-export const getDateRowBackgroundColor = (rowIndex: number, colIndex: number, colorSelect: boolean): string => {
-  if (rowIndex === 0 && colIndex > 2 && colIndex < 9) {
-    return 'lightblue';
-  } else if (rowIndex === 0 && colIndex > 8 && colIndex < 13) {
-    return 'pink';
-  } else if (rowIndex === 0 && colIndex > 12 && colIndex < 15) {
-    return 'lightblue';
-  } else if (rowIndex === 1) {
-    return 'lightblue';
-  } else if (rowIndex === 9) {
-    return 'lightgrey';
-  }
-  return 'white';
 };
--- app/(main)/loan-situation/[kizai_id]/_ui/loan-situation.tsx
+++ app/(main)/loan-situation/[kizai_id]/_ui/loan-situation.tsx
@@ -1,135 +1,214 @@
 'use client';
 
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import {
   Box,
   Button,
+  ClickAwayListener,
   Divider,
   FormControl,
   FormControlLabel,
   FormLabel,
+  Grid2,
   Paper,
+  Popper,
   Radio,
   RadioGroup,
   TextField,
   Typography,
 } from '@mui/material';
-import { Dayjs } from 'dayjs';
+import { addMonths, endOfMonth, subDays, subMonths } from 'date-fns';
+import dayjs, { Dayjs } from 'dayjs';
 import { useState } from 'react';
 
-// import { Calendar } from '../../_ui/date';
-// import GridTable from '../../_ui/gridtable';
-// import {
-//   getDateHeaderBackgroundColor,
-//   getDateHeaderTextColor,
-//   getDateRowBackgroundColor,
-//   getLoanHeaderBackgroundColor,
-//   getLoanRowBackgroundColor,
-//   getLoanTextColor,
-// } from '../_lib/colorselect';
-// import { dateData, dateHeader, dateWidth, loanData, loanHeader, loanWidth } from '../_lib/data';
+import { BackButton } from '@/app/(main)/_ui/buttons';
+import { Calendar, toISOStringWithTimezoneMonthDay } from '@/app/(main)/_ui/date';
+
+import { loanData, useData } from '../_lib/data';
+import { LoanSituationTable, UseTable } from './loan-situation-table';
+
+export type UseData = {
+  id: number;
+  juchuHonbanbiShubetuId: number[];
+};
+
+export type Loan = {
+  juchuHeadId: number;
+  koenNam: string;
+  date: Date | null;
+};
+
+// 使用数テーブルの日付ヘッダーの作成
+const getUseHeader = (date: Date | null) => {
+  const start = date !== null ? subDays(date, 1) : subDays(new Date(), 1);
+  const end = date !== null ? endOfMonth(addMonths(date, 2)) : endOfMonth(addMonths(new Date(), 2));
+  const range: string[] = [];
+  const current = new Date(start);
+
+  while (current <= end) {
+    const dateStr = toISOStringWithTimezoneMonthDay(current).split('T')[0];
+    range.push(dateStr);
+    current.setDate(current.getDate() + 1);
+  }
+
+  return range;
+};
 
 export const LoanSituation = () => {
-  const [visible, setVisible] = useState(false);
+  // 貸出テーブル行配列
+  const [loanRows, setLoanRows] = useState<Loan[]>(loanData);
+  // 使用日テーブルヘッダー
+  const [dateHeader, setDateHeader] = useState<string[]>(getUseHeader(new Date('2025/11/2')));
+  // 使用日テーブルの行配列
+  const [useRows, setStockRows] = useState<UseData[]>(useData);
+  // カレンダー選択日
   const [selectDate, setSelectDate] = useState<Date>(new Date());
 
+  // 保有数
+  const possessions = 200;
+
+  // ポッパー制御
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  /**
+   * 日付選択カレンダー選択時
+   * @param date カレンダー選択日付
+   */
   const handleDateChange = (date: Dayjs | null) => {
     if (date !== null) {
-      setSelectDate(date?.toDate());
+      setSelectDate(date.toDate());
+      const updatedHeader = getUseHeader(date?.toDate());
+      setDateHeader(updatedHeader);
+      // const updatedRow = getStockRow(stock, updatedHeader.length);
+      // setDateHeader(updatedHeader);
+      // const targetIndex: number[] = [];
+      // dateRange.map((targetDate) => {
+      //   updatedHeader.map((date, index) => {
+      //     if (targetDate === date) {
+      //       targetIndex.push(index);
+      //     }
+      //   });
+      // });
+      // setStockRows(updatedRow);
+
+      // setAnchorEl(null);
     }
   };
 
+  // 3か月前
+  const handleBackDateChange = () => {
+    const date = subMonths(new Date(dateHeader[1]), 3);
+    handleDateChange(dayjs(date));
+  };
+  // 3か月後
+  const handleForwardDateChange = () => {
+    const date = addMonths(new Date(dateHeader[1]), 3);
+    handleDateChange(dayjs(date));
+  };
+
+  /**
+   * ポッパー開閉
+   * @param event マウスイベント
+   */
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+  const handleClickAway = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <Paper variant="outlined">
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Typography margin={1}>貸出状況</Typography>
-        <Button sx={{ margin: 1 }}>戻る</Button>
+    <Box>
+      <Box display={'flex'} justifyContent={'end'}>
+        <BackButton label={'戻る'} />
       </Box>
-      <Divider />
-      <Box sx={styles.container}>
-        <Typography marginRight={2}>機材名</Typography>
-        <TextField disabled defaultValue="SHARPY PLUS"></TextField>
-      </Box>
-      <Box sx={styles.container}>
-        <Typography marginRight={2}>保有数</Typography>
-        <TextField
-          disabled
-          defaultValue="200"
-          sx={{
-            '& .MuiInputBase-input': {
-              textAlign: 'right',
-            },
-          }}
-        ></TextField>
-        <Typography marginRight={2} marginLeft={10}>
-          定価
-        </Typography>
-        <TextField
-          disabled
-          defaultValue="20,000円"
-          sx={{
-            '& .MuiInputBase-input': {
-              textAlign: 'right',
-            },
-          }}
-        ></TextField>
-        <FormControl sx={{ marginLeft: 10 }}>
-          <RadioGroup defaultValue="出庫日順">
-            <FormControlLabel value="出庫日順" control={<Radio />} label="出庫日順" />
-            <FormControlLabel value="入庫日順" control={<Radio />} label="入庫日順" />
-          </RadioGroup>
-        </FormControl>
-      </Box>
-      <Box sx={styles.container} justifyContent="center">
-        <Button>＜＜</Button>
-        <Box>
-          <Button sx={{ bgcolor: 'white', color: 'black' }} onClick={() => setVisible(true)}>
-            日付選択
-          </Button>
-          <Box position="absolute" zIndex={1000} display={visible ? 'block' : 'none'}>
-            {/* <Calendar date={selectDate} onChange={handleDateChange} /> */}
-            <Box display="flex" justifyContent="space-between">
-              <Button onClick={() => setVisible(false)} sx={{ margin: 1 }}>
-                キャンセル
-              </Button>
-              <Button onClick={() => setVisible(false)} sx={{ margin: 1 }}>
-                確定
-              </Button>
+      <Paper variant="outlined">
+        <Box display="flex" justifyContent="space-between" alignItems="center" p={2} width="100%">
+          <Typography>貸出状況</Typography>
+        </Box>
+        <Divider />
+        <Box display={'flex'} justifyContent={'end'}>
+          <Button>再表示</Button>
+        </Box>
+        <Grid2 container alignItems={'center'} px={2} py={1} spacing={2}>
+          <Typography>機材名</Typography>
+          <TextField disabled defaultValue="SHARPY PLUS"></TextField>
+        </Grid2>
+        <Grid2 container alignItems={'center'} px={2} py={1} spacing={2}>
+          <Typography>保有数</Typography>
+          <TextField
+            disabled
+            value={possessions}
+            sx={{
+              '& .MuiInputBase-input': {
+                textAlign: 'right',
+              },
+            }}
+          />
+          <Grid2 container alignItems={'center'}>
+            <Typography>定価</Typography>
+            <TextField
+              disabled
+              defaultValue="20,000円"
+              sx={{
+                '& .MuiInputBase-input': {
+                  textAlign: 'right',
+                },
+              }}
+            ></TextField>
+          </Grid2>
+          <Grid2 pl={2}>
+            <FormControl>
+              <RadioGroup defaultValue="出庫日順" row>
+                <FormControlLabel value="出庫日順" control={<Radio />} label="出庫日順" />
+                <FormControlLabel value="入庫日順" control={<Radio />} label="入庫日順" />
+              </RadioGroup>
+            </FormControl>
+          </Grid2>
+        </Grid2>
+        <Box display="flex" flexDirection="row" width="100%">
+          <Box
+            sx={{
+              width: {
+                xs: '40%',
+                sm: '40%',
+                md: '40%',
+                lg: 'min-content',
+              },
+              mt: '62.5px',
+            }}
+          >
+            <LoanSituationTable rows={loanRows} />
+            <Box display={'flex'} justifyContent={'end'}>
+              <Typography>在庫数</Typography>
             </Box>
           </Box>
+          <Box overflow="auto" sx={{ width: { xs: '60%', sm: '60%', md: 'auto' } }}>
+            <Box display="flex" my={2}>
+              <Button onClick={handleBackDateChange}>
+                <ArrowBackIosNewIcon fontSize="small" />
+              </Button>
+              <Button variant="outlined" onClick={handleClick}>
+                日付選択
+              </Button>
+              <Popper open={open} anchorEl={anchorEl} placement="bottom-start">
+                <ClickAwayListener onClickAway={handleClickAway}>
+                  <Paper elevation={3} sx={{ mt: 1 }}>
+                    <Calendar date={selectDate} onChange={handleDateChange} />
+                  </Paper>
+                </ClickAwayListener>
+              </Popper>
+              <Button onClick={handleForwardDateChange}>
+                <ArrowForwardIosIcon fontSize="small" />
+              </Button>
+            </Box>
+            <UseTable header={dateHeader} rows={useRows} possessions={possessions} />
+          </Box>
         </Box>
-        <Button>＞＞</Button>
-      </Box>
-      <Box display="flex" flexDirection="row" width="100%">
-        <Box width="30%">
-          {/* <GridTable
-            header={loanHeader}
-            rows={loanData}
-            editableColumns={null}
-            onChange={undefined}
-            cellWidths={loanWidth}
-            headerColorSelect={true}
-            getHeaderBackgroundColor={getLoanHeaderBackgroundColor}
-            getHeaderTextColor={getLoanTextColor}
-            rowColorSelect={true}
-            getRowBackgroundColor={getLoanRowBackgroundColor}
-          /> */}
-        </Box>
-        <Box width="70%">
-          {/* <GridTable
-            header={dateHeader}
-            rows={dateData}
-            editableColumns={null}
-            onChange={undefined}
-            cellWidths={dateWidth}
-            headerColorSelect={true}
-            getHeaderBackgroundColor={getDateHeaderBackgroundColor}
-            getHeaderTextColor={getDateHeaderTextColor}
-            rowColorSelect={true}
-            getRowBackgroundColor={getDateRowBackgroundColor}
-          /> */}
-        </Box>
-      </Box>
-    </Paper>
+      </Paper>
+    </Box>
   );
 };
 
