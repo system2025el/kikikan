0fea30d9780924c95c722ff11b552d00b8a4e030
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-07-03 11:00:22 +0900

    エラー回避

--- app/(main)/(masters)/locations-master/page.tsx
+++ app/(main)/(masters)/locations-master/page.tsx
@@ -1,9 +1,9 @@
-// import { GetAllLoc } from './_lib/funcs';
+//DB import { GetAllLoc } from './_lib/funcs';
 import { locationList } from './_lib/types';
 import { LocationsMaster } from './_ui/locations-master';
 
 const Page = async () => {
-  // const locs = await GetAllLoc();
+  //DB const locs = await GetAllLoc();
   const locs = locationList;
   return (
     <>
