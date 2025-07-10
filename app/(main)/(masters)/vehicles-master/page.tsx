1899a2621eda99112b2dec8c434485d2a7ff09d1
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-07-10 12:35:37 +0900

    実験

--- app/(main)/(masters)/vehicles-master/page.tsx
+++ app/(main)/(masters)/vehicles-master/page.tsx
@@ -1,11 +1,13 @@
-// import { getAllVehicles } from '@/app/_lib/supabase/supabaseFuncs';
+import { shiori } from '@/app/_lib/postgres/funcs';
+import { getAllVehicles } from '@/app/_lib/supabase/funcs';
 
 import { vehicles } from './_lib/datas';
 import { VehiclesMaster } from './_ui/vehicles-master';
 
 const Page = async () => {
-  // const vehList = await getAllVehicles();
-  const vehList = vehicles;
+  const vehList = await getAllVehicles();
+  // const vehList = await shiori();
+  // const vehList = vehicles;
   return (
     <>
       <VehiclesMaster vehs={vehList} />
