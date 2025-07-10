188f9143e9f4a67beb3e9fa27f9b3e0b2445bdf5
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-07-10 14:21:15 +0900

    fix

--- app/(main)/(masters)/vehicles-master/page.tsx
+++ app/(main)/(masters)/vehicles-master/page.tsx
@@ -1,13 +1,13 @@
-// import { shiori } from '@/app/_lib/postgres/funcs';
-import { getAllVehicles } from '@/app/_lib/supabase/funcs';
+// import { getAllVehicles } from '@/app/_lib/supabase/funcs';
 
-// import { vehicles } from './_lib/datas';
+import { vehicles } from './_lib/datas';
 import { VehiclesMaster } from './_ui/vehicles-master';
+// import { shiori } from '@/app/_lib/postgres/funcs';
 
 const Page = async () => {
-  const vehList = await getAllVehicles();
+  // const vehList = await getAllVehicles();
   // const vehList = await shiori();
-  // const vehList = vehicles;
+  const vehList = vehicles;
   return (
     <>
       <VehiclesMaster vehs={vehList} />
