b63646979a43ca513dac1369e0e49e61a36fbd4e
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-07-24 19:22:37 +0900

    修正

--- app/(main)/(masters)/eqpt-master/_lib/types.ts
+++ app/(main)/(masters)/eqpt-master/_lib/types.ts
@@ -59,3 +59,6 @@ export type EqptsMasterTableValues = {
  * @returns valueが0ならnull, 他はvalueを返す
  */
 export const zeroToNull = <T>(value: T): T | null => (value === 0 ? null : value);
+export const nullToZero = (v: number | null): number => {
+  return v === null ? 0 : v;
+};
