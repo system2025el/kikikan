50a2e43cc48586b8a179337c577eedb4d27c1376
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-07-29 14:46:18 +0900

    確認

--- app/(main)/(masters)/bumons-master/_lib/types.ts
+++ app/(main)/(masters)/bumons-master/_lib/types.ts
@@ -1,15 +1,12 @@
 import { z } from 'zod';
 
-import { zodNumberText } from '../../_lib/membs';
-
 export const BumonsMasterDialogSchema = z.object({
   bumonNam: z.string().max(100, { message: '100文字以内で入力してください' }).min(1, { message: '必須項目です' }),
   delFlg: z.boolean().optional(),
   mem: z.string().max(200, '200文字以内で入力してください').optional(),
-  daibumonId: zodNumberText,
-  shukeibumonId: zodNumberText,
+  daibumonId: z.number({ message: '数字を入力してください' }).optional(),
+  shukeibumonId: z.number({ message: '数字を入力してください' }).optional(),
 });
-
 export type BumonsMasterDialogValues = z.infer<typeof BumonsMasterDialogSchema>;
 
 export type BumonsMasterTableValues = {
