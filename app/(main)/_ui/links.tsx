accfb0bb4ac2d40638219d5fddd1896d05e8d14e
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-08-21 11:46:58 +0900

    サイドバー（入出庫まだ）

--- app/(main)/_ui/links.tsx
+++ app/(main)/_ui/links.tsx
@@ -3,115 +3,139 @@ import ExpandMore from '@mui/icons-material/ExpandMore';
 import StopSharpIcon from '@mui/icons-material/StopSharp';
 import {
   alpha,
+  Box,
   Collapse,
+  Divider,
   Link,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  ListSubheader,
   useTheme,
 } from '@mui/material';
 import { usePathname, useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 
 import { useDirty } from './dirty-context';
 
+/* サイドバーの中身のタイプ */
 type MenuItem = {
   name: string;
   url: string;
 };
-
-const dashboardList: MenuItem[] = [{ name: 'ダッシュボード', url: '/dashboard' }];
+/* 受注管理リスト */
 const orderList: MenuItem[] = [
   { name: '新規受注', url: '/order/0/edit' },
   { name: '受注一覧', url: '/order-list' },
   { name: '見積一覧', url: '/quotation-list' },
   { name: '在庫確認', url: '/stock' },
-  { name: '出庫履歴', url: '/' },
   { name: '貸出状況', url: '/loan-situation' },
 ];
+/* プリントアウトリスト */
 const printList: MenuItem[] = [
-  { name: '出庫指示書', url: '/' },
-  { name: '出庫リスト', url: '/' },
   { name: '納品書', url: '/' },
   { name: '請求書', url: '/' },
 ];
-const rateList: MenuItem[] = [{ name: '稼働率', url: '/' }];
-const equipmentList: MenuItem[] = [{ name: '機材マスタ', url: '/eqpt-master' }];
+/* マスタ管理リスト */
 const masterList: MenuItem[] = [
+  { name: '機材マスタ', url: '/eqpt-master' },
   { name: '顧客マスタ', url: '/customers-master' },
   { name: '車両マスタ', url: '/vehicles-master' },
   { name: '公演場所マスタ', url: '/locations-master' },
-  { name: '担当者マスタ', url: '/users-master' },
-  { name: '一式マスタ', url: '/' },
   { name: '大部門マスタ', url: '/daibumons-master' },
   { name: '集計部門マスタ', url: '/shukeibumons-master' },
   { name: '部門マスタ', url: '/bumons-master' },
   { name: '拠点マスタ', url: '/bases-master' },
   { name: '権限マスタ', url: '/' },
-];
-const settingList: MenuItem[] = [
+  { name: '一式マスタ', url: '/' },
+  { name: '担当者マスタ', url: '/users-master' },
+  { name: '機材セットマスタ', url: '/eqpt-bundle-master' },
   { name: 'マスタインポート', url: '/import-master' },
   { name: 'マスタエクスポート', url: '/export-master' },
 ];
-const loginList: MenuItem[] = [
-  { name: 'ログアウト', url: '/' },
-  { name: 'ログイン管理', url: '/' },
+/* 設定リスト */
+const settingList: MenuItem[] = [];
+/* 入出庫管理リスト */
+const stockIOList: MenuItem[] = [
+  { name: '出庫一覧', url: '/' },
+  { name: '入庫一覧', url: '/' },
+  { name: '移動一覧', url: '/' },
+  { name: 'Weeklyスケジュール', url: '/' },
 ];
+/* ログアウトリスト */
+// const loginList: MenuItem[] = [{ name: 'ログアウト', url: '/' }];
 
+/**
+ * サイドバー
+ * @returns サイドバーのコンポーネント
+ */
 export default function NavLinks() {
+  /* 現在のページの色 */
   const theme = useTheme();
-  const selectedBgColor = alpha(theme.palette.primary.light, 0.2);
+  const currentPgColor = alpha(theme.palette.primary.light, 0.2);
+  /* 現在のページのpath */
   const pathname = usePathname();
+  /*  */
   const router = useRouter();
   const { isDirty } = useDirty();
+  const { requestNavigation } = useDirty();
 
-  const isSelected = (url: string) => {
+  /* useState -------------------------------------------- */
+  /* 受注管理開閉 */
+  const [orderOpen, setorderOpen] = useState(true);
+  /* プリントアウト開閉 */
+  const [printOpen, setprintOpen] = useState(true);
+  /* マスタ管理開閉 */
+  const [masterOpen, setmasterOpen] = useState(true);
+  /* 設定開閉 */
+  const [settingOpen, setsettingOpen] = useState(true);
+  /* 入出庫管理開閉 */
+  const [stockIOOpen, setStockIOOpen] = useState(true);
+  /* ログアウト開閉 */
+  // const [loginOpen, setroginOpen] = useState(true);
+
+  /* methods ----------------------------------------- */
+  /* 現ページとのpath比較 */
+  const isSelected = (url: string): boolean => {
     if (pathname === url /*|| pathname.startsWith(url + '/')*/) {
       return true;
     }
     return false;
   };
-
-  const [orderOpen, setorderOpen] = useState(true);
-  const [printOpen, setprintOpen] = useState(true);
-  const [rateOpen, setrateOpen] = useState(true);
-  const [equipmentOpen, setequipmentOpen] = useState(true);
-  const [masterOpen, setmasterOpen] = useState(true);
-  const [settingOpen, setsettingOpen] = useState(true);
-  const [loginOpen, setroginOpen] = useState(true);
-
+  /* 受注管理クリック時 */
   const orderClick = () => {
     setorderOpen(!orderOpen);
   };
+  /* プリントクリック時 */
   const printClick = () => {
     setprintOpen(!printOpen);
   };
-  const rateClick = () => {
-    setrateOpen(!rateOpen);
-  };
-  const equipmentClick = () => {
-    setequipmentOpen(!equipmentOpen);
-  };
+  /* マスタ管理クリック時 */
   const masterClick = () => {
     setmasterOpen(!masterOpen);
   };
+  /* 設定クリック時 */
   const settingClick = () => {
     setsettingOpen(!settingOpen);
   };
-  const loginClick = () => {
-    setroginOpen(!loginOpen);
+  /* 入出庫管理クリック時 */
+  const stockIOClick = () => {
+    setStockIOOpen(!stockIOOpen);
   };
-
-  const { requestNavigation } = useDirty();
+  /* ログアウトクリック時 */
+  // const loginClick = () => {
+  //   setroginOpen(!loginOpen);
+  // };
 
   return (
     <List dense sx={{ pt: 0 }}>
+      {/* ダッシュボード */}
       <ListItemButton
         href="/dashboard"
         sx={{
-          backgroundColor: pathname === '/dashboard' ? selectedBgColor : '',
+          backgroundColor: pathname === '/dashboard' ? currentPgColor : '',
         }}
       >
         <ListItemIcon>
@@ -119,6 +143,7 @@ export default function NavLinks() {
         </ListItemIcon>
         <ListItemText sx={{ color: pathname === '/dashboard' ? 'primary.dark' : '' }}>ダッシュボード</ListItemText>
       </ListItemButton>
+      {/* 受注管理 */}
       <ListItemButton onClick={orderClick}>
         <ListItemIcon>
           <StopSharpIcon />
@@ -133,7 +158,7 @@ export default function NavLinks() {
               key={text.name}
               disablePadding
               sx={{
-                backgroundColor: isSelected(text.url) ? selectedBgColor : '',
+                backgroundColor: isSelected(text.url) ? currentPgColor : '',
               }}
             >
               <ListItemButton /*href={text.url}*/ onClick={() => requestNavigation(text.url)} dense>
@@ -143,6 +168,7 @@ export default function NavLinks() {
           ))}
         </List>
       </Collapse>
+      {/* プリントアウト */}
       <ListItemButton onClick={printClick}>
         <ListItemIcon>
           <StopSharpIcon />
@@ -157,7 +183,7 @@ export default function NavLinks() {
               key={text.name}
               disablePadding
               sx={{
-                backgroundColor: isSelected(text.url) ? selectedBgColor : '',
+                backgroundColor: isSelected(text.url) ? currentPgColor : '',
               }}
             >
               <ListItemButton href={text.url} dense>
@@ -167,54 +193,7 @@ export default function NavLinks() {
           ))}
         </List>
       </Collapse>
-
-      <ListItemButton onClick={rateClick}>
-        <ListItemIcon>
-          <StopSharpIcon />
-        </ListItemIcon>
-        <ListItemText>稼働率</ListItemText>
-        {rateOpen ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
-      <Collapse in={rateOpen} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItem
-            disablePadding
-            sx={{
-              backgroundColor: pathname === '/kadouritu' ? selectedBgColor : '',
-            }}
-          >
-            <ListItemButton href="/kadouritu" dense>
-              <ListItemText primary="稼働率" sx={{ color: pathname === '/kadouritu' ? 'primary.dark' : '', pl: 8 }} />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Collapse>
-
-      <ListItemButton onClick={equipmentClick}>
-        <ListItemIcon>
-          <StopSharpIcon />
-        </ListItemIcon>
-        <ListItemText>機材マスタ</ListItemText>
-        {equipmentOpen ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
-      <Collapse in={equipmentOpen} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItem
-            disablePadding
-            sx={{
-              backgroundColor: pathname === '/eqpt-master' ? selectedBgColor : '',
-            }}
-          >
-            <ListItemButton href="/eqpt-master" dense>
-              <ListItemText
-                primary="機材マスタ"
-                sx={{ color: pathname === '/eqpt-master' ? 'primary.dark' : '', pl: 8 }}
-              />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Collapse>
-
+      {/* マスタ管理 */}
       <ListItemButton onClick={masterClick}>
         <ListItemIcon>
           <StopSharpIcon />
@@ -224,22 +203,25 @@ export default function NavLinks() {
       </ListItemButton>
       <Collapse in={masterOpen} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
-          {masterList.map((text) => (
-            <ListItem
-              key={text.name}
-              disablePadding
-              sx={{
-                backgroundColor: isSelected(text.url) ? selectedBgColor : '',
-              }}
-            >
-              <ListItemButton href={text.url} dense>
-                <ListItemText primary={text.name} sx={{ color: isSelected(text.url) ? 'primary.dark' : '', pl: 8 }} />
-              </ListItemButton>
-            </ListItem>
+          {masterList.map((text, index) => (
+            <Fragment key={text.name}>
+              <ListItem
+                key={text.name}
+                disablePadding
+                sx={{
+                  backgroundColor: isSelected(text.url) ? currentPgColor : '',
+                }}
+              >
+                <ListItemButton href={text.url} dense>
+                  <ListItemText primary={text.name} sx={{ color: isSelected(text.url) ? 'primary.dark' : '', pl: 8 }} />
+                </ListItemButton>
+              </ListItem>
+              {index === 11 && <Divider sx={{ ml: 8 }} />}
+            </Fragment>
           ))}
         </List>
       </Collapse>
-
+      {/* 設定 */}
       <ListItemButton onClick={settingClick}>
         <ListItemIcon>
           <StopSharpIcon />
@@ -254,7 +236,7 @@ export default function NavLinks() {
               key={text.name}
               disablePadding
               sx={{
-                backgroundColor: isSelected(text.url) ? selectedBgColor : '',
+                backgroundColor: isSelected(text.url) ? currentPgColor : '',
               }}
             >
               <ListItemButton href={text.url} dense>
@@ -264,8 +246,36 @@ export default function NavLinks() {
           ))}
         </List>
       </Collapse>
-
-      <ListItemButton onClick={loginClick}>
+      {/* 入出庫管理 */}
+      <ListItemButton onClick={stockIOClick}>
+        <ListItemIcon>
+          <StopSharpIcon />
+        </ListItemIcon>
+        <ListItemText>入出庫管理</ListItemText>
+        {stockIOOpen ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      {/* <Collapse in={stockIOOpen} timeout="auto" unmountOnExit>
+        <List disablePadding>
+          <ListSubheader>
+            <Box>出庫</Box>
+          </ListSubheader>
+          {stockIOList.map((text) => (
+            <ListItem
+              key={text.name}
+              disablePadding
+              sx={{
+                backgroundColor: isSelected(text.url) ? currentPgColor : '',
+              }}
+            >
+              <ListItemButton href={text.url} dense>
+                <ListItemText primary={text.name} sx={{ color: isSelected(text.url) ? 'primary.dark' : '', pl: 8 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Collapse> */}
+      {/* ログアウト */}
+      {/* <ListItemButton onClick={loginClick}>
         <ListItemIcon>
           <StopSharpIcon />
         </ListItemIcon>
@@ -279,7 +289,7 @@ export default function NavLinks() {
               key={text.name}
               disablePadding
               sx={{
-                backgroundColor: isSelected(text.url) ? selectedBgColor : '',
+                backgroundColor: isSelected(text.url) ? currentPgColor : '',
               }}
             >
               <ListItemButton href={text.url} dense>
@@ -288,7 +298,17 @@ export default function NavLinks() {
             </ListItem>
           ))}
         </List>
-      </Collapse>
+      </Collapse> */}
+      <ListItemButton href="/">
+        <ListItemIcon>
+          <StopSharpIcon />
+        </ListItemIcon>
+        <ListItemText>ログアウト</ListItemText>
+      </ListItemButton>
+      {/* 一番下が見づらいので空白 */}
+      <ListItem>
+        <Box height={50}></Box>
+      </ListItem>
     </List>
   );
 }
