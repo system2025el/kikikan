057bfef97e3dd6f8e2b56ce6baa783cafc901773
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-09-04 14:23:26 +0900

    データ読み込み方法変更

--- app/(main)/_ui/loading.tsx
+++ app/(main)/_ui/loading.tsx
@@ -9,3 +9,24 @@ export const Loading = () => {
     </Box>
   );
 };
+
+export const LoadingOverlay = () => {
+  return (
+    <Box
+      sx={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(255,255,255,0.6)',
+        zIndex: 10,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <CircularProgress size={'100px'} />
+    </Box>
+  );
+};
