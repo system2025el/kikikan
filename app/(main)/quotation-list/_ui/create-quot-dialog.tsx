3071405506cbcc7ac7c2136e68ca7da98c0d0db9
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-09-30 15:59:17 +0900

    請求UI、見積のコード共通化

---
+++ app/(main)/quotation-list/_ui/create-quot-dialog.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { Button, DialogActions, DialogTitle, Stack,Typography } from '@mui/material';
+import { useRouter } from 'next/navigation';
+import router from 'next/router';
+import { useRef } from 'react';
+import { TextFieldElement, useForm } from 'react-hook-form-mui';
+
+import { CloseMasterDialogButton } from '../../_ui/buttons';
+
+export const CreateQuotDialog = ({
+  setDialogOpen,
+}: {
+  setDialogOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const router = useRouter();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  /* methods ------------------------------------- */
+  /* 自動生成ボタン押下 */
+  const onSubmit = (data: { juchuHeadId: number | null }) => {
+    console.log(data.juchuHeadId, 'の見積もりを自動生成');
+    router.push(`/quotation-list/create?juchuId=${data.juchuHeadId}`);
+  };
+
+  /* useForm ------------------------------------- */
+  const { control, handleSubmit } = useForm<{ juchuHeadId: number | null }>({
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
+    defaultValues: { juchuHeadId: null },
+  });
+  return (
+    <>
+      <DialogTitle display={'flex'} justifyContent={'space-between'}>
+        受注番号から自動生成
+        <CloseMasterDialogButton handleCloseDialog={() => setDialogOpen(false)} />
+      </DialogTitle>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack p={4}>
+          <Typography>受注番号</Typography>
+          <TextFieldElement
+            name={'juchuHeadId'}
+            control={control}
+            inputRef={inputRef}
+            rules={{
+              required: '数字を入力してください',
+            }}
+            sx={{
+              '& .MuiInputBase-input': {
+                textAlign: 'right',
+              },
+              '& input[type=number]::-webkit-inner-spin-button': {
+                WebkitAppearance: 'none',
+                margin: 0,
+              },
+            }}
+            type="number"
+          />
+        </Stack>
+        <DialogActions>
+          <Button type="submit">自動生成</Button>
+          <Button
+            onClick={() => {
+              setDialogOpen(false);
+              router.push('/quotation-list/create');
+            }}
+          >
+            手動生成
+          </Button>
+        </DialogActions>
+      </form>
+    </>
+  );
+};
