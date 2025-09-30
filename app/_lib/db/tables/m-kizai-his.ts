305ff855e4ac4ee067b2c041f61068ddf644f444
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-09-30 13:03:43 +0900

    エクセルインポートエクスポート処理変更

--- app/_lib/db/tables/m-kizai-his.ts
+++ app/_lib/db/tables/m-kizai-his.ts
@@ -14,7 +14,7 @@ import { SCHEMA } from '../supabase';
 export const insertEqptHistory = async (data: EqptsMasterDialogValues, id: number) => {
   const query = `
           INSERT INTO ${SCHEMA}.m_kizai_his (
-            kizai_id_his_num, kizai_id, kizai_nam, del_flg, section_num, el_num, shozoku_id,
+            kizai_id_his_num, kizai_id, kizai_nam, del_flg, section_num, shozoku_id,
             bld_cod, tana_cod, eda_cod, kizai_grp_cod, dsp_ord_num, mem,
             bumon_id, shukei_bumon_id, dsp_flg, ctn_flg, def_dat_qty,
             reg_amt, rank_amt_1, rank_amt_2, rank_amt_3, rank_amt_4, rank_amt_5, add_user, add_dat, upd_user, upd_dat
@@ -23,7 +23,7 @@ export const insertEqptHistory = async (data: EqptsMasterDialogValues, id: numbe
           VALUES (
             (SELECT coalesce(max(kizai_id_his_num),0) + 1 FROM ${SCHEMA}.m_kizai_his),
             $1, $2, $3, $4, $5, $6, $7, $8, $9,
-            $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26 ,$27
+            $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26
           );
         `;
   const values = [
@@ -31,7 +31,6 @@ export const insertEqptHistory = async (data: EqptsMasterDialogValues, id: numbe
     data.kizaiNam,
     Number(data.delFlg),
     data.sectionNum,
-    data.elNum,
     zeroToNull(data.shozokuId),
     data.bldCod,
     data.tanaCod,
