201fc3aa138e973f61ccae8aa86d519d61d3ffe5
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-10-20 17:20:14 +0900

    テスト

--- app/(main)/(masters)/masters-import/_ui/masters-import.tsx
+++ app/(main)/(masters)/masters-import/_ui/masters-import.tsx
@@ -65,6 +65,10 @@ export const ImportMaster = () => {
       if (type === 'eqpt') {
         const parsedEqptData: EqptImportType[] = [];
         let hasError = false;
+        console.log(
+          'どこいったｎ',
+          dataRows.map((d) => d[0])
+        );
         dataRows.forEach((row, index) => {
           // rfid_tag_idの部分が空ならそもそも取得しない
           if (!row[0] || row[0].trim() !== '') return;
