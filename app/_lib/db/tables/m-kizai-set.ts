1f39b29cef9fe9e6765de4e26fb0a838cbef7794
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-10-27 11:30:19 +0900

    機材選択いったんあげ

--- app/_lib/db/tables/m-kizai-set.ts
+++ app/_lib/db/tables/m-kizai-set.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import pool from '../postgres';
-import { SCHEMA } from '../supabase';
+import { SCHEMA, supabase } from '../supabase';
 
 /**
  * 選択された機材のセット機材のIDリストを取得する関数
@@ -9,17 +9,8 @@ import { SCHEMA } from '../supabase';
  * @returns 選択された機材のセット機材のIDリスト
  */
 export const selectBundledEqptIds = async (idList: number[]) => {
-  const placeholders = idList.map((_, i) => `$${i + 1}`).join(',');
-  const query = `
-    SELECT DISTINCT
-      set_kizai_id
-    FROM
-      ${SCHEMA}.m_kizai_set
-    WHERE
-      kizai_id IN (${placeholders})
-  `;
   try {
-    return await pool.query(query, idList);
+    return await supabase.schema(SCHEMA).from('m_kizai_set').select('kizai_id, set_kizai_id').in('kizai_id', idList);
   } catch (e) {
     throw e;
   }
