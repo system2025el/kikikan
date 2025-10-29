87a27e51a9ab195062fdfb077f5905fcb86b33ff
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-10-29 16:07:01 +0900

    顧客マスタから顧客ランクを削除し、値引き率追加

--- app/(main)/(masters)/customers-master/_lib/types.ts
+++ app/(main)/(masters)/customers-master/_lib/types.ts
@@ -12,11 +12,6 @@ export const CustomersMaterDialogSchema = z.object({
     .max(100, { message: validationMessages.maxStringLength(100) })
     .min(1, { message: validationMessages.required() }),
   delFlg: z.boolean().optional(),
-  // keisho: z
-  //   .string()
-  //   .max(10, { message: validationMessages.maxStringLength(10) })
-  //   .nullish(),
-  kokyakuRank: z.number().nullish(),
   adrPost: z
     .string()
     .max(20, { message: validationMessages.maxStringLength(20) })
@@ -55,10 +50,7 @@ export const CustomersMaterDialogSchema = z.object({
     .max(200, { message: validationMessages.maxStringLength(200) })
     .nullish(),
   dspFlg: z.boolean().optional(),
-  nebikiRat: z.number().nullish(),
-  // closeDay: z.number({ message: validationMessages.number() }).nullable(),
-  // siteDay: z.number({ message: validationMessages.number() }).nullable(),
-  // kizaiNebikiFlg: z.boolean().optional(),
+  nebikiRat: z.number({ message: validationMessages.number() }).int({ message: '整数で入力してください' }).nullish(),
 });
 export type CustomersMasterDialogValues = z.infer<typeof CustomersMaterDialogSchema>;
 
