d791867115641160adae9d1cf75d378b39d89fc3
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-11-06 11:42:34 +0900

    fix

--- app/(main)/nyuko-list/page.tsx
+++ app/(main)/nyuko-list/page.tsx
@@ -3,7 +3,7 @@ import { NyukoListSearchValues } from './_lib/types';
 import { NyukoList } from './_ui/nyuko-list';
 
 const Page = async () => {
-  const nyukoSearch: NyukoListSearchValues = { juchuHeadId: null, shukoDat: new Date(), shukoBasho: 0 };
+  const nyukoSearch: NyukoListSearchValues = { juchuHeadId: null, shukoDat: new Date(), shukoBasho: 0, section: [] };
   const nyukoData = await getNyukoList(nyukoSearch);
   return <NyukoList shukoData={nyukoData} />;
 };
