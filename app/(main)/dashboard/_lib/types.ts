45d1599700f34c5857b4af85d0c16e4314158a0a
R01P224\h.yokota <h.yokota@refact.co.jp>
2025-11-17 18:07:27 +0900

    ダッシュボード追加、修正

---
+++ app/(main)/dashboard/_lib/types.ts
@@ -0,0 +1,33 @@
+/**
+ * ダッシュボード用のテーブル型
+ */
+//出庫時間未設定、車両未設定
+export type DashboardTableValues = {
+  calDat: string;
+  kizaiHeadId: number | null;
+  juchuHeadId: number | null;
+  headNam: string | null;
+  koenNam: string | null;
+  koenbashoNam: string | null;
+  kokyakuNam: string | null;
+  yardShukoDat: string | null;
+  kicsShukoDat: string | null;
+  yardNyukoDat: string | null;
+  kicsNyukoDat: string | null;
+};
+
+//マイナス在庫
+export type MinusZaikoValues = {
+  kizaiId: number;
+  kizaiNam: string | null;
+  kizaiQty: number | null;
+  kizaiNgQty: number | null;
+  bumonNam: string | null;
+  daibumonNam: string | null;
+  delFlg: boolean | null;
+  dspFlg: boolean | null;
+  mem: string | null;
+  regAmt: number | null;
+  shozokuNam: string | null;
+  shukeibumonNam: string | null;
+};
