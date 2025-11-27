640f49a950d9ed6a9437e7649393b9fb2234c59f
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-11-27 11:53:07 +0900

    いったん

---
+++ app/_lib/db/tables/v-juchu-sharyo-head-lst.ts
@@ -0,0 +1,16 @@
+'use server';
+
+import { SCHEMA, supabase } from '../supabase';
+
+/**
+ * 受注ヘッドIDが一致する受注車両ヘッダのリストを取得する関数
+ * @param {number} juchuHeadId 受注ヘッダID
+ * @returns {} 受注車両ヘッダのデータ配列
+ */
+export const selectJuchuSharyoHeadList = async (juchuHeadId: number) => {
+  try {
+    return supabase.schema(SCHEMA).from('v_juchu_sharyo_head_lst').select('*').eq('juchu_head_id', juchuHeadId);
+  } catch (e) {
+    throw e;
+  }
+};
