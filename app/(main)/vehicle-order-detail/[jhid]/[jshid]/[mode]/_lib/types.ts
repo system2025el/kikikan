2fc5f4d8da28c89639f70abee7a86fb8461067f5
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-12-01 18:33:08 +0900

    受注車両ヴァリデーション追加

--- app/(main)/vehicle-order-detail/[jhid]/[jshid]/[mode]/_lib/types.ts
+++ app/(main)/vehicle-order-detail/[jhid]/[jshid]/[mode]/_lib/types.ts
@@ -1,32 +1,69 @@
 import z from 'zod';
 
+import { FAKE_NEW_ID } from '@/app/(main)/(masters)/_lib/constants';
+
 import { validationMessages } from '../../../../../_lib/validation-messages';
 
 /**
  * 受注車両ヘッダ入力フォームのスキーマ
  */
-export const JuchuSharyoHeadSchema = z.object({
-  juchuHeadId: z.number(),
-  juchuShryoHeadId: z.number(),
-  headNam: z
-    .string({ message: validationMessages.required() })
-    .min(1, { message: validationMessages.required() })
-    .max(20, { message: validationMessages.maxStringLength(20) }),
-  nyushukoKbn: z.number(),
-  nyushukoDat: z.date({ message: validationMessages.required() }).nullable(),
-  nyushukoBashoId: z.number({ message: validationMessages.required() }).min(0),
-  headMem: z.string().nullable(),
-  meisai: z.array(
-    z.object({
-      sharyoId: z.number().nullable(),
-      sharyoQty: z
-        .number({ message: validationMessages.number() })
-        .int({ message: validationMessages.int() })
-        .nullable(),
-      sharyoMem: z.string().nullable(),
-    })
-  ),
-});
+export const JuchuSharyoHeadSchema = z
+  .object({
+    juchuHeadId: z.number(),
+    juchuShryoHeadId: z.number(),
+    headNam: z
+      .string({ message: validationMessages.required() })
+      .min(1, { message: validationMessages.required() })
+      .max(20, { message: validationMessages.maxStringLength(20) }),
+    nyushukoKbn: z
+      .number({ message: validationMessages.required() })
+      .min(0, { message: validationMessages.required() }),
+    nyushukoDat: z
+      .date({ message: validationMessages.required() })
+      .nullable()
+      .refine((dat) => dat, { message: validationMessages.required() }),
+    nyushukoBashoId: z
+      .number({ message: validationMessages.required() })
+      .min(0, { message: validationMessages.required() }),
+    headMem: z.string().nullable(),
+    meisai: z.array(
+      z
+        .object({
+          sharyoId: z.number().nullable(),
+          sharyoQty: z
+            .number({ message: validationMessages.number() })
+            .int({ message: validationMessages.int() })
+            .nullable(),
+          sharyoMem: z.string().nullable(),
+        })
+        .superRefine((data, ctx) => {
+          if (data.sharyoQty && data.sharyoQty > 0 && (!data.sharyoId || data.sharyoId < 0)) {
+            ctx.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: '車両を選択してください',
+              path: ['sharyoId'],
+            });
+          }
+          if (data.sharyoId && data.sharyoId > 0 && (!data.sharyoQty || data.sharyoQty <= 0)) {
+            ctx.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: '必須',
+              path: ['sharyoQty'],
+            });
+          }
+        })
+    ),
+  })
+  .refine(
+    (args) => {
+      const { meisai } = args;
+      const validItems = meisai.filter(
+        (item) => item.sharyoQty && item.sharyoQty > 0 && item.sharyoId && item.sharyoId > 0
+      );
+      return validItems.length > 0;
+    },
+    { message: '最低1台登録してください', path: ['meisai'] }
+  );
 // .refine((data) => data.meisai.v1Id && data.meisai.v1Id > 0, {
 //   message: '入力してください',
 //   path: ['meisai.v1Id'],
