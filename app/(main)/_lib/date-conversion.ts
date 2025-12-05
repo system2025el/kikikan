bd46c5c9cd4ae62c3e5ec8618237771cd3fc0e6a
shiori.takahashi <shiori.takahashi@refact.co.jp>
2025-12-05 22:10:11 +0900

    weeklyいったん

--- app/(main)/_lib/date-conversion.ts
+++ app/(main)/_lib/date-conversion.ts
@@ -1,3 +1,5 @@
+import 'dayjs/locale/ja';
+
 import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
@@ -5,6 +7,7 @@ import utc from 'dayjs/plugin/utc';
 // .tz()を使う準備
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.locale('ja');
 
 /**
  * 日本時間の年月日時分文字列に変換する関数
@@ -25,6 +28,15 @@ export const toJapanYMDString = (input: Date | string | number = new Date(), str
   return dayjs(input).tz('Asia/Tokyo').format(`YYYY${str}MM${str}DD`);
 };
 /**
+ * 日本時間の年月日曜日を文字列に変換する関数
+ * @param {Date | string | number} input 引数無なら現在時刻 Date string number
+ * @param {string} str 時間の間に入る記号
+ * @returns {string} 日本時間の文字列 YYYY${str}MM${str}DD (dd)
+ */
+export const toJapanYMDAndDayString = (input: Date | string | number = new Date(), str: string = '/'): string => {
+  return dayjs(input).tz('Asia/Tokyo').locale('ja').format(`YYYY${str}MM${str}DD (dd)`);
+};
+/**
  * 日本時間の月日文字列に変換する関数
  * @param {Date | string | number} input 引数無なら現在時刻 Date string number
  * @param {string} str 時間の間に入る記号
@@ -33,6 +45,16 @@ export const toJapanYMDString = (input: Date | string | number = new Date(), str
 export const toJapanMDString = (input: Date | string | number = new Date(), str: string = '/'): string => {
   return dayjs(input).tz('Asia/Tokyo').format(`MM${str}DD`);
 };
+
+/**
+ * 日本時間の文字列に変換する関数
+ * @param input 引数無なら現在時刻 Date string number
+ * @returns 日本時間の文字列 HH:mm
+ */
+export const toJapanHHmmString = (input: Date | string | number = new Date()): string => {
+  return dayjs(input).tz('Asia/Tokyo').format(`HH:mm`);
+};
+
 /**
  * 日本時間のtimestamp型 文字列に変換する関数
  * @param input 引数無なら現在時刻 Date string number
