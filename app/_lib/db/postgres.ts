1131bd43392a1957fa2c9cfbdc75645e73fca2fc
s.kashiwai <s.kashiwai@refact.co.jp>
2025-12-09 11:33:21 +0900

    pool シングルトン

--- app/_lib/db/postgres.ts
+++ app/_lib/db/postgres.ts
@@ -1,7 +1,17 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// export default pool;
+
+const globalForPool = global as unknown as { pool?: Pool };
+
+export const pool =
+  globalForPool.pool ??
+  (globalForPool.pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  }));
 
 export default pool;
