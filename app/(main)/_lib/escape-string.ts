28f084a962fed33eae3758bdc86f18a249322c6b
s.kashiwai <s.kashiwai@refact.co.jp>
2026-03-10 14:46:51 +0900

    あいまい検索：.orを使用している部分には専用のエスケープ処理追加

--- app/(main)/_lib/escape-string.ts
+++ app/(main)/_lib/escape-string.ts
@@ -2,3 +2,8 @@ export const escapeLikeString = (str: string) => {
   // 「%」「_」「\」をバックスラッシュでエスケープ＋トリム
   return str.replace(/[\\%_]/g, '\\$&').trim();
 };
+
+export const escapeOrLikeString = (str: string) => {
+  // 「%」「_」「\」をバックスラッシュでエスケープ＋トリム
+  return str.replace(/[\\%_]/g, '\\\\$&').trim();
+};
