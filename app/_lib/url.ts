779c5c5d815b1e199d873a24cb828836bd2cb546
s.kashiwai <s.kashiwai@refact.co.jp>
2026-03-16 17:01:58 +0900

    テスト

--- app/_lib/url.ts
+++ app/_lib/url.ts
@@ -8,15 +8,16 @@ export const getUrl = () => {
   // return url.endsWith('/') ? url : `${url}/`;
 
   console.log('本番？', process.env.VERCEL_PROJECT_PRODUCTION_URL);
+  console.log('場所:', process.env.NEXT_PUBLIC_VERCEL_ENV);
 
   // 本番環境固定URL
-  if (process.env.VERCEL_ENV === 'production') {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
     console.log('本番');
     return 'https://kikikan-psi.vercel.app/';
   }
 
   // 自動生成されたURL
-  if (process.env.VERCEL_ENV === 'preview') {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
     console.log('開発');
     return `https://${process.env.VERCEL_URL}`;
   }
