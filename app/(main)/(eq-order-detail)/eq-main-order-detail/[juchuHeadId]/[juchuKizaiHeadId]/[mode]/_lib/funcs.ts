18edb6f9f3777f5cfadb56e960bd9ab4029dd780
s.kashiwai <s.kashiwai@refact.co.jp>
2026-07-29 13:20:25 +0900

    修正

--- app/(main)/(eq-order-detail)/eq-main-order-detail/[juchuHeadId]/[juchuKizaiHeadId]/[mode]/_lib/funcs.ts
+++ app/(main)/(eq-order-detail)/eq-main-order-detail/[juchuHeadId]/[juchuKizaiHeadId]/[mode]/_lib/funcs.ts
@@ -2257,6 +2257,7 @@ export const confirmHonbanbi = async (
       }
       throw error;
     }
+    return true;
   } catch (e) {
     if (e instanceof Error) {
       console.error(`[ERROR] ${e.message}`);
